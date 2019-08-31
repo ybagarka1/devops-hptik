1bdcc1a8b83fb2df1d9d12381333853f96c1b60d
Yash Bagarka <yash.bagarka@travelex.com>
2019-08-31 13:46:35 +0000

    resolved the 4 questions

--- q1/shell_commands.sh
+++ q1/shell_commands.sh
@@ -12,22 +12,15 @@ https://www.cyberciti.biz/faq/howto-show-mysql-open-database-connections-on-linu
 
 #iii. Bash command to get value of version number of 3 decimal points (first occurrence) from a file containing the JSON:
 
-JSON_DATA='"{
-
-"name": "abc",
-
-"version": "1.0",
-
-"version": "1.0.57",
-
-"description": "Testing",
-
-"main": "src/server/index.js",
-
-"version": "1.1"
-
-}"'
-
 cat file.json| grep -Po '"version":.*?[^\\]",'
 
 echo ${JSON_DATA} | grep -Po '"vetsion\":\"(\.[a-zA-Z.@]+)\",'
+
+#iv.
+
+
+NUMBERS="0.0238063905753, 0.0308368914424,0.0230014918637, 0.0274232220275, 0.0184563749986"
+
+cat iv.txt
+
+awk '{ total += $1; count++ } END { printf ("%2.2f", total/count) }' iv.txt
