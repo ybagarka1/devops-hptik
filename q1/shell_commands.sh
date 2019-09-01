a998185ec8d6ba140b693f9bf76a8308d1b1ad5f
Yash Bagarka <yash.bagarka@travelex.com>
2019-09-01 06:07:43 +0000

    added the json regex match

--- q1/shell_commands.sh
+++ q1/shell_commands.sh
@@ -2,24 +2,20 @@
 
 #i. Kill all processes/zombie processes of service called “gunicorn” in a single command.
 
-#echo "kill -9 $(echo $(ps aux | grep 'Z' | grep gunicorn |  awk '{ print $2 }'))"
+echo "kill -9 $(echo $(ps aux | grep 'Z' | grep gunicorn |  awk '{ print $2 }'))"
 
 
 #ii. MySQL shell command to show the unique IPs from where MySQL connections are being made to the Database.
 
-https://www.cyberciti.biz/faq/howto-show-mysql-open-database-connections-on-linux-unix/
+
+mysql -u root -BNe "select host,count(host) from processlist;" information_schema
 
 
 #iii. Bash command to get value of version number of 3 decimal points (first occurrence) from a file containing the JSON:
 
-cat file.json| grep -Po '"version":.*?[^\\]",'
+cat file.json| grep -Po '"version":.*?[^\\.]{2}",'
 
-echo ${JSON_DATA} | grep -Po '"vetsion\":\"(\.[a-zA-Z.@]+)\",'
-
-#iv.
-
-
-NUMBERS="0.0238063905753, 0.0308368914424,0.0230014918637, 0.0274232220275, 0.0184563749986"
+#iv. Bash command to add these numbers from a file and find average upto 2 decimal points:
 
 cat iv.txt
 
